94aa3cb3e7c86f0869cfdeaaee6ac4cdc9723142
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-03-04 23:11:17 -0600

    add Footer Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BasicLayoutComponent } from './layouts/basic-layout/basic-layout.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { BasicLayoutComponent } from './layouts/basic-layout/basic-layout.compon
     NavbarComponent,
     HomeComponent,
     BasicLayoutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
